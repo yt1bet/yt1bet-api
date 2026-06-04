e7b18ab4b7a0f56b51654ffd46725ad6e73f3212
yt1bet <hashividiq@gmail.com>
2026-06-04 07:14:32 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { exec } = require('child_process');
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 function run(cmd) {
